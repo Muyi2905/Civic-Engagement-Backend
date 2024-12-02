f0557bed9843b27593a827800943ae160e63e547
Muyi2905 <obaremimuyiwa@gmail.com>
2024-12-02 09:26:32 +0100

    grouped api

--- models/user.go
+++ models/user.go
@@ -8,11 +8,11 @@ import (
 
 type User struct {
 	gorm.Model
-	FirstName string    `gorm:"not null" json:"first_name" validate:"required,min=2,max=50"`           // User's first name
-	LastName  string    `gorm:"not null" json:"last_name" validate:"required,min=2,max=50"`            // User's last name
-	Email     string    `gorm:"uniqueIndex;not null" json:"email" validate:"required,email"`           // Email must be unique and valid
-	Username  string    `gorm:"uniqueIndex;not null" json:"username" validate:"required,min=3,max=30"` // Username must be unique
-	Password  string    `gorm:"not null" json:"password,omitempty" validate:"required,min=6"`          // Password must have a minimum length
-	LastLogin time.Time `gorm:"default:NULL" json:"last_login,omitempty"`                              // Tracks the last login time
-	IsActive  bool      `gorm:"default:true" json:"is_active"`                                         // Indicates if the user is active
+	FirstName string    `gorm:"default:''" json:"first_name" validate:"omitempty,min=2,max=50"`
+	LastName  string    `gorm:"default:''" json:"last_name" validate:"omitempty,min=2,max=50"`
+	Email     string    `gorm:"uniqueIndex;not null" json:"email" validate:"required,email"`
+	Username  string    `gorm:"uniqueIndex;not null" json:"username" validate:"required,min=3,max=30"`
+	Password  string    `gorm:"not null" json:"password,omitempty" validate:"required,min=6"`
+	LastLogin time.Time `gorm:"default:NULL" json:"last_login,omitempty"`
+	IsActive  bool      `gorm:"default:true" json:"is_active"`
 }
--- routes/electedofficials.go
+++ routes/electedofficials.go
@@ -6,21 +6,26 @@ import (
 	"muyi2905/civic/backend/controllers"
 )
 
-// officialRoutes sets up the routes for ElectedOfficial CRUD operations
 func OfficialRoutes(db *gorm.DB, r *gin.Engine) {
-	r.POST("/officials", func(c *gin.Context) {
-		controllers.CreateOfficial(db, c)
-	})
-	r.GET("/officials", func(c *gin.Context) {
-		controllers.GetOfficials(db, c)
-	})
-	r.GET("/officials/:id", func(c *gin.Context) {
-		controllers.GetOfficial(db, c)
-	})
-	r.PUT("/officials/:id", func(c *gin.Context) {
-		controllers.UpdateOfficial(db, c)
-	})
-	r.DELETE("/officials/:id", func(c *gin.Context) {
-		controllers.DeleteOfficial(db, c)
-	})
+	api := r.Group("/api")
+	{
+		officials := api.Group("/officials")
+		{
+			officials.POST("", func(c *gin.Context) {
+				controllers.CreateOfficial(db, c)
+			})
+			officials.GET("", func(c *gin.Context) {
+				controllers.GetOfficials(db, c)
+			})
+			officials.GET("/:id", func(c *gin.Context) {
+				controllers.GetOfficial(db, c)
+			})
+			officials.PUT("/:id", func(c *gin.Context) {
+				controllers.UpdateOfficial(db, c)
+			})
+			officials.DELETE("/:id", func(c *gin.Context) {
+				controllers.DeleteOfficial(db, c)
+			})
+		}
+	}
 }
